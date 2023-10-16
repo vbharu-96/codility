11463bdc8a862301a945a667ea4a882b83806557
vbharu-96 <117460860+vbharu-96@users.noreply.github.com>
2023-10-16 11:13:25 +0200

    Create MaxSliceSum.cpp

---
+++ MaxSliceSum.cpp
@@ -0,0 +1,16 @@
+//A non-empty array A consisting of N integers is given. A pair of integers (P, Q), 
+//such that 0 ≤ P ≤ Q < N, is called a slice of array A. The sum of a slice (P, Q) is the total of A[P] + A[P+1] + ... + A[Q].
+#include<iostream>
+#define INM -2147483648
+int MaxSliceSum(vector<int> &A) {
+    // Implement your solution here
+    int max_e =0,max_s= INM ;
+    auto max = [](int x,int y){
+        return (x>y)?x:y;
+    };
+    for(auto i : A){
+        max_e = max(i,max_e+i);
+        max_s = max(max_e,max_s);
+    }
+    return max_s;
+}
